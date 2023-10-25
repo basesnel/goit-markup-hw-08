3564baf58fb61b4ba3bd4204349653cd11f8466d
basesnel <basesnel@gmail.com>
2023-10-25 11:51:47 +0300

    Added getClientSize function to get cross-browser dimensions of the visible part of the page

--- js/modal.js
+++ js/modal.js
@@ -9,12 +9,37 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  function getClientSize() {
+    if (typeof window.innerWidth == 'number') {
+      //Non-IE
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      return { width, height };
+    }
+
+    if (
+      document.documentElement &&
+      (document.documentElement.clientWidth || document.documentElement.clientHeight)
+    ) {
+      //IE 6+ in 'standards compliant mode'
+      const width = document.documentElement.clientWidth;
+      const height = document.documentElement.clientHeight;
+      return { width, height };
+    }
+
+    if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+      //IE 4 compatible
+      const width = document.body.clientWidth;
+      const height = document.body.clientHeight;
+      return { width, height };
+    }
+  }
+
   function toggleModal() {
     document.body.classList.toggle('modal-open');
     refs.backDrop.classList.toggle('backdrop--is-hidden');
 
-    // console.log(window.innerHeight, refs.modal.clientHeight);
-    if (refs.modal.clientHeight <= window.innerHeight) {
+    if (refs.modal.clientHeight <= getClientSize().height) {
       refs.modal.classList.toggle('modal--absolute');
     }
   }
